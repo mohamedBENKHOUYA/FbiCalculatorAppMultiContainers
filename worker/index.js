457a9119d9b5377b926960ef8055fc1f81dbf209
mohamed ben khouya <mohamed.benkhouya.1995@gmail.com>
2021-02-26 00:42:54 +0100

    created serverAPi, Worker and ReactFromEndApp

---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require("./keys");
+
+const redis = require("redis");
+
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+    if(index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+	//any time that we get a new value that shows up in redis 
+sub.on("message", (channel, message) => {
+    // insert each value into hash called values
+    redisClient.hset('values', message, fib(parseInt(message)))
+});
+
+sub.subscribe('insert');
